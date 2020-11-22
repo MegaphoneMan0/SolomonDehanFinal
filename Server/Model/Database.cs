736912d6e6a5985577cee3f1429658e88336884e
John Solomon <jackfrost0@ksu.edu>
2020-11-22 17:43:18 -0600

    I messed up the BidLibrary, but I'm pretty sure it's fixed now
    Testing

--- Server/Model/Database.cs
+++ Server/Model/Database.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Server.Model
 {
     class Database
     {
-    }
+
+        private User[] userLibrary = new User[2];
+
+        //private Product[]
+
+
+    }//database
 }
