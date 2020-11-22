b83fe535078359beb5aab9beda5af0a35b68eaf3
John Solomon <jackfrost0@ksu.edu>
2020-11-22 17:17:33 -0600

    Add project files.

---
+++ BidLibrary/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BidLibrary
+{
+    //John
+    class Message
+    {
+    }
+}
---
+++ BidLibrary/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BidLibrary
+{
+    //jack
+    class Product
+    {
+    }
+}
