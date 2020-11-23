10a623a3fb776e0f31baf8841b52d4112d054780
John Solomon <jackfrost0@ksu.edu>
2020-11-22 20:47:34 -0600

    Working on the communicator, but I don't know how to do the websockets so that's awkward

--- Server/Controller/Communicator.cs
+++ Server/Controller/Communicator.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BidLibrary;
+using BidLibrary.Library;
+using System.Net.WebSockets;
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace Server.Controller
 {
-    class Communicator
+    /// <summary>
+    /// handles communicating information to and from connected clients
+    /// </summary>
+    class Communicator 
     {
+        //properties
+
+        /// <summary>
+        /// interface that allows the communicator to call the "readmessage" method from the controller  
+        /// </summary>
+        private ReadMessage readMessageHandler;
+
+
+        //methods
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Communicator()
+        {
+
+        }//communicator
+
+        /// <summary>
+        /// This method will use websockets to send all connected clients the updated list of products
+        /// </summary>
+        /// <param name="products"></param>
+        public void sendToClients(List<Product> products)
+        {
+            //no idea how to do this yet
+        }//sendToClients
+
+
+        public void OnMessage(MessageEventArgs messageEventArgs)
+        {
+
+        }
+
+
     }
 }
