5cc04ab804ee735380bb3ffb0230ad7173ab8c9e
John Solomon <jackfrost0@ksu.edu>
2020-11-22 18:06:03 -0600

    Was going to work on Database, but I need to finish the data types first

--- Server/Model/Database.cs
+++ Server/Model/Database.cs
@@ -18,5 +18,34 @@ namespace Server.Model
 
         private List<Bid> bidLibrary = new List<Bid>();
 
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Database()
+        {
+
+        }//Database
+
+        /// <summary>
+        /// This method returns a product with the matching ID
+        /// </summary>
+        /// <param name="ID">The StringID of the desired Product</param>
+        /// <returns>The desired product. If this product does not exist return is "null"</returns>
+        public Product search(string ID)
+        {
+            foreach(Product p in productLibrary)
+            {
+                //need to implament Product before I can make this work
+            }//foreach
+
+            return null;
+        }//Product
+
+
+
+
+
+
     }//database
 }
