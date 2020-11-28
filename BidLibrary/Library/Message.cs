2a460ff1d548c6d01c53d98cd4f33f6e621e2c64
John Solomon <jackfrost0@ksu.edu>
2020-11-28 11:05:18 -0600

    Built out a bare-bones message so that the server can have the correct logic

--- BidLibrary/Library/Message.cs
+++ BidLibrary/Library/Message.cs
@@ -6,8 +6,77 @@ using System.Threading.Tasks;
 
 namespace BidLibrary.Library
 {
-    //john
+    
     public class Message
     {
+        
+        private MessageType type;
+
+        private string userName;
+
+        private string password;
+
+        private List<Product> products;
+
+        private Bid newBid;
+
+        private bool credentialVerification;
+
+
+        //methods
+
+        public Message(MessageType mType, string name, string pass)
+        {
+
+        }
+
+        public Message(MessageType mType, string name, string pass, bool verification)
+        {
+
+        }
+
+        public Message(MessageType mType, List<Product> lProduct)
+        {
+
+        }
+
+        public Message(MessageType mType, Product product, Bid bid)
+        {
+
+        }
+
+        public MessageType getMessageType()
+        {
+
+        }
+
+        public string getUserName()
+        {
+
+        }
+
+        public string getPassword()
+        {
+
+        }
+
+        public List<Product> getProducts()
+        {
+
+        }
+
+        public Bid getNewBid()
+        {
+
+        }
+
+        public bool getCredentialVerification()
+        {
+
+        }
+
+
+
+
     }
 }
