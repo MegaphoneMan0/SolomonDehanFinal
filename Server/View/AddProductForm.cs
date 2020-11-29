59185463746ce3e9c76e12d7478994c1bec31d5b
John Solomon <jackfrost0@ksu.edu>
2020-11-28 23:46:32 -0600

    I actually think that addproductform is done, now I need to finish the observer bs

--- Server/View/AddProductForm.cs
+++ Server/View/AddProductForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BidLibrary.Library;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,8 +18,26 @@ namespace Server.View
             InitializeComponent();
         }
 
+        //just hard coded these into the listbox's "items" property
+        /*
+        private static List<Product> productsThatCanBeAdded = new List<Product>()
+        {
+            new Product("Iphone 4s"),
+            new Product("Civilization Revolution"),
+            new Product("Goldfish Packet"),
+            new Product("The entire Russian government")
+        };
+        */
+
+        ProductUpdater productUpdaterHandler;
+
         private void uxAddButton_Click(object sender, EventArgs e)
         {
+
+            Product newProduct = new Product(uxProductsToAddBox.SelectedItem.ToString());
+
+            productUpdaterHandler.UpdateProduct(newProduct);
+            
 
         }
     }
